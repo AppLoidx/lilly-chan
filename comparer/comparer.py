52a683f12aaacf2b33bdd5f5af64db30f7104e47
Arthur <apploidyakutsk@gmail.com>
2018-10-22 21:54:56 +0900

    added body

--- comparer/comparer.py
+++ comparer/comparer.py
@@ -1,2 +1,46 @@
-class Compare():
-    pass
+class Compare:
+
+    @staticmethod
+    def compare(name: str, array: list, upper: bool = True) -> bool:
+        """
+        Сравнивает значение переданного слова со значениями массива. Так же учитваются возможные опечатки,
+        но только позиционно. То есть каждая позиция проверяется с соответвующей.
+
+        :param name: проверяемое слово
+        :param array: массив, где хранятся возможные значения слова
+        :param upper: если истина, то не обращает внимания на регистр, иначе различает
+        :return: если хотя бы одно значение с массива совпадает со словом, возращает True, иначе False
+        """
+        if upper:
+            name = name.upper()
+            for i in range(len(array)):
+                array[i] = array[i].upper()
+
+        for i in array:
+            k = 0  # считывание разницы в символах (посимвольно, позиционно)
+            if len(i) > len(name):
+                for j in range(len(name)):
+                    if name[j] == i[j]:
+                        pass
+                    else:
+                        k = k + 1
+            else:
+                for j in range(len(i)):
+                    if i[j] == name[j]:
+                        pass
+                    else:
+                        k = k + 1
+
+            k = k + abs(len(i) - len(name))  # добавление разницы в недостающих символах
+
+            # Обработка возможной опечатки
+            if 7 > len(name) > 4 and k < 3:
+                return True
+            elif 7 <= len(name) < 12 and k < 5:
+                return True
+            elif len(name) > 11 and k < 7:
+                return True
+            elif len(name) <= 4 and k < 1:
+                return True
+
+        return False
